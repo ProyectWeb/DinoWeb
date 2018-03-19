e3b593d85da3812e42c69cf5e926146174882991
Adrian11Herrera <Adrian-097@outlook.com>
2018-03-19 16:12:22 -0600

    creacion archivo php

---
+++ templates/Adrian/Nosotros.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Nosotros</title>
+
+</head>
+<body>
+<?php
+
+print"Acerca de Nosotros<br>";
+
+
+
+
+
+
+
+?>	
+</body>
+</html>
