0ab932a0dff1944538f4f719451814617dbea231
Laura <lvcm.lvcm.lc@outlook.com>
2018-03-25 23:40:06 -0600

    Plantilla para catalogo de la pagina: creacion de menu, carousel y tarjetas

---
+++ templates/Laura/pack/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+"use strict";
+
+
+$('.slider-active').owlCarousel({
+    loop:true,
+    nav:true,
+    navText:['Back','Next'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+})(jQuery);
