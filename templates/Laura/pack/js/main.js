c772bc1aa7a772dc55187c3105d44cf0817faa5b
Laura <lvcm.lvcm.lc@outlook.com>
2018-03-26 18:43:28 -0600

    segundo avance del template, falta la parte resposiva

--- templates/Laura/pack/js/main.js
+++ templates/Laura/pack/js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
 "use strict";
 
-
+//carousel
 $('.slider-active').owlCarousel({
     loop:true,
     nav:true,
@@ -15,6 +15,52 @@ $('.slider-active').owlCarousel({
         },
         1000:{
             items:1
+        }
+    }
+})
+
+
+//tarjetas
+$('.product-active').owlCarousel({
+    loop:true,
+    nav:true,
+    margin:30,
+    navText:['Back','Next'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+
+
+//Iconos que maybe quite
+$('.brand-active').owlCarousel({
+    loop:true,
+    nav:false,
+    margin:30,
+    navText:['Back','Next'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        992:{
+            items:5
+        },
+        1200:{
+            items:6
         }
     }
 })
