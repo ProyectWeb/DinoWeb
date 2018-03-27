0e03fd0b27fe6a3542832d0b5a1306b1ac19352c
Laura <lvcm.lvcm.lc@outlook.com>
2018-03-27 03:21:05 -0600

    He arreglado la parte responsiva y tambien se agrega la plantilsecundaria para la tienda

--- templates/Laura/pack/js/main.js
+++ templates/Laura/pack/js/main.js
@@ -1,6 +1,15 @@
 (function ($) {
 "use strict";
 
+
+	jQuery('#mobile-menu').meanmenu({
+		meanMenuContainer: '.mobile-menu',
+		meanScreenWidth: "767"
+
+	});
+
+
+
 //carousel
 $('.slider-active').owlCarousel({
     loop:true,
@@ -28,13 +37,16 @@ $('.product-active').owlCarousel({
     navText:['Back','Next'],
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:false,
         },
-        600:{
-            items:1
+        768:{
+            items:2
         },
-
-        1000:{
+        992:{
+            items:4
+        },
+        1200:{
             items:4
         }
     }
@@ -51,10 +63,10 @@ $('.brand-active').owlCarousel({
     navText:['Back','Next'],
     responsive:{
         0:{
-            items:1
+            items:2
         },
-        600:{
-            items:1
+        768:{
+            items:3
         },
         992:{
             items:5
@@ -64,6 +76,11 @@ $('.brand-active').owlCarousel({
         }
     }
 })
+
+
+
+//select
+$('select').niceSelect();
 
 
 
