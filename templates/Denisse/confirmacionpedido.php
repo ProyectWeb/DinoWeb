c2a9e18e0d0837c11e4375f69289d6d4f002c480
Richi Di <drdr_2@hotmail.com>
2018-04-04 03:40:44 -0500

    Solved issues on confirmaciondelpedido and added classes for styles

--- templates/Denisse/confirmacionpedido.php
+++ templates/Denisse/confirmacionpedido.php
@@ -1,84 +1,65 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<html>
 
 <head>
 
-    <meta charset="utf-8">
-    <meta name="robots" content="all,follow">
-    <meta name="googlebot" content="index,follow,snippet,archive">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Obaju e-commerce template">
-    <meta name="author" content="Ondrej Svestka | ondrejsvestka.cz">
-    <meta name="keywords" content="">
-
-    <title>
-        confirmaciondelpedido
-    </title>
-
-    <meta name="keywords" content="">
 
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100' rel='stylesheet' type='text/css'>
 
     <!-- styles -->
     <link href="../../static/css/font-awesome.css" rel="stylesheet">
-    <link href="../../static/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../static/css/catalogoestilos/bootstrap.css" rel="stylesheet">
     <link href="../../static/css/animate.min.css" rel="stylesheet">
     <link href="../../static/css/owl.carousel.css" rel="stylesheet">
     <link href="../../static/css/owl.theme.css" rel="stylesheet">
 
-    <!-- theme stylesheet -->
-    <link href="../../static/css/style.default.css" rel="stylesheet" id="theme-stylesheet">
+    <!--Own Styles-->
+    <link rel="stylesheet" href="../../static/css/infodelpedido.css">
 
     <!-- your stylesheet with modifications -->
     <link href="../../static/css/custom.css" rel="stylesheet">
 
     <script src="../../static/javascript/base/respond.min.js"></script>
 
-  <!--  <link rel="shortcut icon" href="favicon.png">-->
+    <!--  <link rel="shortcut icon" href="favicon.png">-->
 
 
 
 </head>
 
 <body>
-<?php
-include('../Richi/navBar.html');
-?>
-    <!-- *** TOPBAR ***
- _________________________________________________________ -->
+    <!--NavBar-->
+    <?php
+        include('../Richi/navBar.php');
+    ?>
+        <!--Fin NavBar-->
 
-
-    <!-- *** TOP BAR END *** -->
-
-    <!-- *** NAVBAR ***
- _________________________________________________________ -->
-
-  
-                            
-
-    <!-- *** NAVBAR END *** -->
-
-    <div id="all">
-
-        <div id="content">
-            <div class="container">
-
-                <div class="col-md-12">
-                   <!--Barra de navegación--> 
-
-                </div>
-
-                <div class="col-md-9" id="checkout">
-
+        <div class="container">
+            <div class="row">
+                <div class="card col-12" id="checkout">
                     <div class="box">
-                        <form method="post" action="Pago.html">
-                            <h1 align ="center">Confirmación del pedido</h1>
+                        <form method="post" action="Pago.php">
+                            <h1 align="center">Confirmación del pedido</h1>
                             <ul class="nav nav-pills nav-justified">
-                                <li><a href="infodelpedido.html"><i class="fa fa-user"></i><br>Datos</a>
+                                <li class="btn-sld col-4">
+                                    <a href="infodelpedido.php">
+                                        <i class="fa fa-user"></i>
+                                        <br>Datos</a>
                                 </li>
-                                <li><a href="Pago.html"><i class="fa fa-money"></i><br>Método de pago</a>
+                                <li class="btn-sld col-4">
+                                    <a href="Pago.php">
+                                        <i class="fa fa-money"></i>
+                                        <br>Método de pago</a>
                                 </li>
-                                <li class="active"><a href="#"><i class="fa fa-eye"></i><br>Confirmación de pedido</a>
+                                <li class="btn-sld active col-4">
+                                    <a href="#">
+                                        <i class="fa fa-eye"></i>
+                                        <br>Confirmación de pedido</a>
                                 </li>
                             </ul>
 
@@ -91,118 +72,61 @@ include('../Richi/navBar.html');
                                                 <th>Cantidad</th>
                                                 <th>Precio</th>
                                                 <th>Total</th>
-                                                </tr>
-                                                </thead>
-                                                <tbody>
-                                                <tr>
-                                                <td>
-                                                    <a href="#">
-                                                       <!-- <img src="img/detailsquare.jpg" alt="White Blouse Armani">-->
-                                                    </a>
-                                                </td>
-                                                <td>  <!--<a href="#">White Blouse Armani</a>-->
-                                                </td>
-                                                <td>      </td>
-                                                <td> </td>
-                                                <td>  </td>
-                                                <td></td>
                                             </tr>
+                                        </thead>
+                                        <tbody>
                                             <tr>
                                                 <td>
                                                     <a href="#">
-                                                      <!--  <img src="img/basketsquare.jpg" alt="Black Blouse Armani">-->
+                                                        <!-- <img src="img/detailsquare.jpg" alt="White Blouse Armani">-->
                                                     </a>
                                                 </td>
-                                                <td><a href="#">          </a>
+                                                <td>
+                                                    <!--<a href="#">White Blouse Armani</a>-->
                                                 </td>
-                                                <td>       </td>
-                                                <td>       </td>
-                                                <td>       </td>
-                                                <td>       </td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
                                             </tr>
                                         </tbody>
                                         <tfoot>
                                             <tr>
                                                 <th colspan="5">Total</th>
-                                                <th>         </th>
+                                                <th> </th>
                                             </tr>
                                         </tfoot>
                                     </table>
-
                                 </div>
                                 <!-- /.table-responsive -->
                             </div>
                             <!-- /.content -->
 
-                            <div class="box-footer">
+                            <div>
                                 <div class="pull-left">
-                                    <a href="Pago.html" class="btn btn-default"><i class="fa fa-chevron-left"></i>Cancelar</a>
+                                    <a href="Pago.php" class="btn btn-default">
+                                        <button type="submit" class="btn btn-primary">Atras</button>
+                                    </a>
                                 </div>
                                 <div class="pull-right">
-                                    <button type="submit" class="btn btn-primary">Continuar<i class="fa fa-chevron-right"></i>
-                                    </button>
+                                    <button type="submit" class="btn btn-primary">Finalizar</button>
                                 </div>
                             </div>
                         </form>
                     </div>
-                    <!-- /.box -->
-
-
                 </div>
-                <!-- /.col-md-9 -->
-
-                <div class="col-md-3">
-
-                   
-
-
-                </div>
-                <!-- /.col-md-3 -->
-
             </div>
-            <!-- /.container -->
         </div>
-        <!-- /#content -->
 
-
-        <!-- *** FOOTER ***
- _________________________________________________________ -->
-      
-        <!-- /#footer -->
-
-        <!-- *** FOOTER END *** -->
-
-
-
-
-        <!-- *** COPYRIGHT ***
- _________________________________________________________ -->
-       
-        <!-- *** COPYRIGHT END *** -->
-
-
-
-    </div>
-    <!-- /#all -->
-
-
-    
-
-    <!-- *** SCRIPTS TO INCLUDE ***
- _________________________________________________________ -->
-    <script src="../../static/javascript/base/jquery-1.11.0.min.js"></script>
-    <script src="../../static/javascript/base/bootstrap.min.js"></script>
-    <script src="../../static/javascript/base/jquery.cookie.js"></script>
-    <script src="../../static/javascript/base/waypoints.min.js"></script>
-    <script src="../../static/javascript/base/modernizr.js"></script>
-    <script src="../../static/javascript/base/bootstrap-hover-dropdown.js"></script>
-    <script src="../../static/javascript/base/owl.carousel.min.js"></script>
-    <script src="../../static/javascript/base/front.js"></script>
-
-
-
-
-
+        <!--SCRIPTS TO INCLUDE-->
+        <script src="../../static/javascript/base/jquery-1.11.0.min.js"></script>
+        <script src="../../static/javascript/base/bootstrap.min.js"></script>
+        <script src="../../static/javascript/base/jquery.cookie.js"></script>
+        <script src="../../static/javascript/base/waypoints.min.js"></script>
+        <script src="../../static/javascript/base/modernizr.js"></script>
+        <script src="../../static/javascript/base/bootstrap-hover-dropdown.js"></script>
+        <script src="../../static/javascript/base/owl.carousel.min.js"></script>
+        <script src="../../static/javascript/base/front.js"></script>
 
 </body>
 
